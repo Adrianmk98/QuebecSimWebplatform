cba33726b92f30822cdabfb9622228f1994c5014
Adrian <adrianmkrzyzanowski@gmail.com>
2025-03-01 18:42:48 -0500

    -Reworked the post display

--- viewPost.php
+++ viewPost.php
@@ -17,6 +17,9 @@ if (!(isset($_SESSION['loggedin'])))//FLIP AFTER DONE
 
     <title>Profile
     </title>
+    <link rel="stylesheet" href="tableStyle.css">
+    <link rel="stylesheet" href="headerStyle.css">
+    <link rel="stylesheet" href="partyStyle.css">
     <div class="card">
     <?php
     $stmt = $db_link->prepare("SELECT pressID,pressFlairID, authorID,pressTitle,pressContent FROM press WHERE pressID = ?");
@@ -26,11 +29,29 @@ if (!(isset($_SESSION['loggedin'])))//FLIP AFTER DONE
 
     if ($result->num_rows > 0) {
         while ($row = $result->fetch_assoc()) {
+            $playerID=$row['authorID'];
+            $AuthorName="SELECT cname from user where ID=$playerID";
+            $result = $db_link->query($AuthorName)or die($db_link->error);
+            $username = $result->fetch_assoc();
+            $username = $username['cname'];
+
+            if($row['pressFlairID']==1)
+            {
+                $flairname="Press";
+            }
+            if($row['pressFlairID']==2)
+            {
+                $flairname="Government Business";
+            }
+            if($row['pressFlairID']==3)
+            {
+                $flairname="Media";
+            }
             echo "<li><h1>";
-            echo $row['pressTitle'];
-            echo "</h1>".$row['pressFlairID'];
-            echo "By". $row['authorID'];
-            echo $row['pressContent'];
+            echo $row['pressTitle']."<br>";
+            echo "</h1><h3>".$flairname."</h3><br>";
+            echo "By ". $username."<br>";
+            echo $row['pressContent']."<br>";
             echo "<br></li>";
         }
         echo "</ul>";
